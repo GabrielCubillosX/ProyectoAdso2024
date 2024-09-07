e19a0aeb08c88bbed0c64d7ab450546046329bc2
GabrielCubillosX <gabrielestebancubillos@gmail.com>
2024-09-07 11:54:32 -0500

    controladores creados y todo funcionando

--- app/Http/Controllers/ModelosAutoController.php
+++ app/Http/Controllers/ModelosAutoController.php
@@ -8,4 +8,31 @@ use Illuminate\Http\Request;
 class ModelosAutoController extends Controller
 {
     //
+    public function getData (Request $request){
+        $rta=10+20;
+        return response()->json([
+            'status' => '200',
+            'message' => 'data...',
+            'result'=> $rta
+        ]);
+    }
+    public function update (Request $request){
+        return response()->json([
+            'status' => '200',
+            'message' => 'guardado con exito',
+        ]);
+    }
+    public function save (Request $request){
+        return response()->json([
+            'status' => '200',
+            'message' => 'guardado con exito',
+        ]);
+    }
+    public function delete (Request $request){
+        return response()->json([
+            'status' => '200',
+            'message' => 'guardado con exito',
+        ]);
+    }
+
 }
